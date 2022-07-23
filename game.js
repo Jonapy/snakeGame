0b16e4d7b728b19c613e1e610c8c984b12f1858c
Thomas Cinalli <tcinalli12@gmail.com>
2022-07-22 19:03:21 -0600

    This is it :( another dot

--- game.js
+++ game.js
@@ -1,56 +1,52 @@
-
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-let x = canvas.width/2;
-let y = canvas.height - 30;
-let ballRadius = 10;
-let dist = 0.5;
-let rightBound = canvas.width - ballRadius;
-let lowBound = canvas.height - ballRadius;
-let direction = moveUp;
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+function drawBall( xCor, yCor, color ) {
+    ctx.beginPath( );
+    ctx.arc( xCor, yCor, ballRadius, 0, Math.PI*2 );
+    ctx.fillStyle = color;
+    ctx.fill( );
+    ctx.closePath( );
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+function draw( ) {
+    // Make same size as ball
+    //ctx.clearRect( 0, 0, canvas.width, canvas.height );  
+    drawBall( x, y, '0095DD' );
     // put a function that adds to the right direction
-    direction();
+    direction( );
 }
 
-function moveRight(){
+function findFood( ) {
+    let foodX = Math.floor( Math.random( ) * x );
+    let foodY = Math.floor( Math.random( ) * y ); 
+    drawBall( foodX, foodY, '000000' );
+}
+
+function moveRight( ) {
     x += dist; 
 }
 
-function moveLeft(){
+function moveLeft( ) {
     x -= dist;
 }
 
-function moveUp(){
+function moveUp( ) {
     y -= dist;
 }
 
-function moveDown(){
+function moveDown( ) {
     y += dist;
 }
 
-function changeDirection(e) {
-    if(e.key == "ArrowRight" && x < rightBound) {
+function changeDirection( e ) {
+    if( e.key == "ArrowRight" && x < rightBound ) {
         // move right
         direction = moveRight; 
     }
-    if(e.key == "ArrowLeft" && x > ballRadius ) {
+    if( e.key == "ArrowLeft" && x > ballRadius ) {
         // move left
         direction = moveLeft;
     }
-    if(e.key == "ArrowUp" && y > ballRadius) {
-        // move up
+    if( e.key == "ArrowUp" && y > ballRadius ) {
+        // move up 
         direction = moveUp;
     }
     if( e.key == "ArrowDown" && y < lowBound ) {
@@ -59,31 +55,21 @@ function changeDirection(e) {
     }
 }
 
-document.addEventListener("keydown", changeDirection, false);
+document.addEventListener( "keydown", changeDirection, false );
+setInterval( draw, 10 );
 
-setInterval(draw, 10);
-/*
-ctx.beginPath();
-// draws a rectangle/square
-// coordinate of top left corner
-// then width and height
 
-ctx.rect(20, 40, 50, 50);
-// saves shape color
-ctx.fillStyle = "#FF0000";
+let canvas = document.getElementById( "myCanvas" );
+let ctx = canvas.getContext( "2d") ;
 
-// fill up the shape with the color
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-ctx.beginPath();
-// coordinates of the center of the arc
-// radius, initial and final angle, 
-    ctx.arc(240, 160, 20, 0, Math.PI, false);
-    ctx.fillStyle = "green";
-    //ctx.fill();
-    // fi you want to draw the external part of shape
-    ctx.strokeStyle = "green"
-    ctx.stroke();
-ctx.closePath();
-*/
+let ballRadius = 10;
+let dist = 0.5;
+
+let rightBound = canvas.width - ballRadius;
+let lowBound = canvas.height - ballRadius;
+
+let direction = moveUp;
+findFood( );
