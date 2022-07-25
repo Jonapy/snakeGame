b51b174cdfe43922a766e7ea9c4b4e58d5c6ed85
Jonathan Canossa Valerio <jonacanovale@gmail.com>
2022-07-24 21:20:38 -0600

    made snake go smoother, add losing conditions

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
-document.addEventListener( "keydown", changeDirection, false );
-setInterval( draw, 500 );
-
+// Queue used to keep track of the snake's tail
 class Queue {
   constructor( ) {
     this.elements = { };
@@ -28,33 +26,6 @@ class Queue {
   }
 }
 
-let canvas = document.getElementById( "myCanvas" );
-let ctx = canvas.getContext( "2d") ;
-
-let x = canvas.width/2;
-let y = canvas.height/2;
-
-let ballRadius = 10;
-let dist = ballRadius * 2;
-
-let rightBound = canvas.width - ballRadius;
-let lowBound = canvas.height - ballRadius;
-
-let direction = moveUp;
-
-//let map = ( new Array( 480 ) ).fill( ( new Array( 320 ) ).fill( 0 ) );	// 2D array representing the map with 0's
-let map = [];
-for(let i = 0; i < 480;i++){
-  map[i] = [];
-  for(let j = 0; j < 320;j++){
-    map[i][j] = 0;
-  }
-}
-
-let snakeQ = new Queue( );
-
-findFood( );
-
 function drawBall( xCor, yCor, color ) {
     ctx.beginPath( );
     ctx.arc( xCor, yCor, ballRadius, 0, Math.PI*2 );
@@ -63,18 +34,25 @@ function drawBall( xCor, yCor, color ) {
     ctx.closePath( );
 }
 
-function draw( ) {
-     //Make same size as ball
+function snakeLogic( ) {
     if ( snakeQ.length( ) >= 4 ) {
-          let tempList = snakeQ.dequeue( );
-          ctx.clearRect( tempList[ 0 ] - ballRadius, tempList[ 1 ] - ballRadius, ballRadius * 2, ballRadius * 2 );
-        map[ tempList[ 0 ] ][ tempList[ 1 ] ] = 0;
+      let tempList = snakeQ.dequeue( );
+      ctx.clearRect( tempList[ 0 ] - ballRadius, tempList[ 1 ] - ballRadius, ballRadius * 2, ballRadius * 2 );
+      map[ tempList[ 0 ] ][ tempList[ 1 ] ] = 0;
     }
 
     snakeQ.enqueue( [ x, y ] );
-    console.log(`map[${x}][${y}] = ${map[x][y]}`);
-    map[ x ][ y ] = 2;
+    //console.log(`map[${x}][${y}] = ${map[x][y]}`);
+    console.log("coordinates:"+x+" "+y); //y = 320 is dead, x = 480
+    
     drawBall( x, y, '0095DD' );
+    // check game status
+    if (!gameStatus()){
+      ctx.strokeText("You lost!", canvas.height/2, canvas.height/2);
+      ctx.strokeText("Play again?", canvas.height/2 , canvas.height/2 + canvas.height/4);
+      //window.location.reload();
+    }
+    map[ x ][ y ] = 2;
     // put a function that adds to the right direction
     direction( );
 }
@@ -123,3 +101,42 @@ function changeDirection( e ) {
         direction = moveDown;
     }
 }
+
+function gameStatus() {
+  // if anything here is true, user lost
+  if ( map[x][y] === 2 || x <= 0 || x >= 480 || y <= 0 || y >= 320 ) {
+    console.log("in");
+    return false;
+  }
+  // user is still alive
+  return true;
+}
+
+// main
+let canvas = document.getElementById( "myCanvas" );
+let ctx = canvas.getContext( "2d" );
+ctx.font = "30px Arial";
+let x = canvas.width/2;
+let y = canvas.height/2;
+let ballRadius = 5;
+let dist = ballRadius * 2;
+let rightBound = canvas.width - ballRadius;
+let lowBound = canvas.height - ballRadius;
+let snakeQ = new Queue( );
+let direction = moveUp;
+
+//let map = ( new Array( 480 ) ).fill( ( new Array( 320 ) ).fill( 0 ) );	// 2D array representing the map with 0's
+let map = [];
+for(let i = 0; i < 480;i++){
+  map[i] = [];
+  for(let j = 0; j < 320;j++){
+    map[i][j] = 0;
+  }
+}
+
+findFood( );
+
+// listen to key's pressed
+document.addEventListener( "keydown", changeDirection, false );
+
+setInterval( snakeLogic, 225 );
